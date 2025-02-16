d007f335f5c6ca1db02b368ddfc529b97940dd8b
thisisfaizanali <faizanaliwhb@gmail.com>
2025-02-16 23:59:44 +0530

    update core logic

--- Travel Tracker/index.js
+++ Travel Tracker/index.js
@@ -4,11 +4,12 @@ import pg from "pg";
 
 const app = express();
 const port = 3000;
+
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "faiz999",
+  password: "",
   port: 5432,
 });
 db.connect();
@@ -16,16 +17,56 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
-  const result = await db.query("select country_code from visited_countries");
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
 });
 
 app.listen(port, () => {
